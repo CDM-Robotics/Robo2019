f8378cbfc4620fdaf9a2d7496339c54a5e4025c5
austin leehealey <austinleehealey@gmail.com>
2019-03-02 09:36:45 -0800

    Opened Watch dog for wrist

--- src/main/java/team6072/robo2019/subsystems/WristSys.java
+++ src/main/java/team6072/robo2019/subsystems/WristSys.java
@@ -203,10 +203,10 @@ public class WristSys extends Subsystem {
 
             setSensorStartPosn();
 
-            // set the watch dog going
-            mWatchDogTimer = new Timer("WristSys watchdog");
-            // wait for 1 second before starting, then check every 50 milliseconds
-            mWatchDogTimer.schedule(mWatchDog, 1000, 50);
+            // // set the watch dog going
+            // mWatchDogTimer = new Timer("WristSys watchdog");
+            // // wait for 1 second before starting, then check every 50 milliseconds
+            // mWatchDogTimer.schedule(mWatchDog, 1000, 50);
 
             mLog.info("WristSys ctor  complete -------------------------------------");
         } catch (Exception ex) {
